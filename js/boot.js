37432713bf8b233fecb00538e5766bfb4abdf9bb
heliesinb <grahammqxs35@gmail.com>
2024-12-11 17:14:08 +0800

    Site updated: 2024-12-11 17:14:07

--- js/boot.js
+++ js/boot.js
@@ -17,6 +17,22 @@ Fluid.boot.refresh = function() {
   Fluid.events.refresh();
 };
 
+/* Sidebar */
+var toggleSidebar = function(){
+  $("#sidebar").toggleClass('sidebar-hide');
+  $("#toggle-sidebar").toggleClass('sidebar-button-shift');
+}
+var hideSidebar = function(){
+  $("#sidebar").addClass('sidebar-hide');
+  $("#toggle-sidebar").addClass('sidebar-button-shift');
+}
+$("#toggle-sidebar").on("click",toggleSidebar);
+$("header").on("click",hideSidebar);
+$("#mainContent").on("click",hideSidebar);
+$("#footerContent").on("click",hideSidebar);
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   Fluid.boot.registerEvents();
 });
